776a9ef00260e9482ecf06ac70f1c9988a2fd49c
Michael Rudolph <michael.rudolph@enit.io>
2022-11-10 17:02:06 +0100

    Add failing test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link timeout raises', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = await screen.findByText(/learn react7/i);
   expect(linkElement).toBeInTheDocument();
-});
+}, 1);
+
+
+test('renders learn react link timeout not raises', async () => {
+  render(<App />);
+  const linkElement = await screen.findByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+}, 1);
